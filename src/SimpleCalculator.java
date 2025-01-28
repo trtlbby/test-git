643683f6fbfac03668eeb18da983477136feefef
trtlbby <earllawrence.bacsain@unc.edu.ph>
2025-01-28 17:39:12 +0800

    added a calculator

---
+++ src/SimpleCalculator.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class SimpleCalculator {
+
+    
+    public static void main(String[] args) {
+        double result;
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Input number");
+        System.out.println("Input number");
+    }
+}
