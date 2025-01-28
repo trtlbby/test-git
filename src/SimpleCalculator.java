08d1cd46f14c7c167a67e255d5a94e828c323b49
trtlbby <earllawrence.bacsain@unc.edu.ph>
2025-01-28 17:49:21 +0800

    Calculator Ver 1

--- src/SimpleCalculator.java
+++ src/SimpleCalculator.java
@@ -4,10 +4,29 @@ public class SimpleCalculator {
 
     
     public static void main(String[] args) {
-        double result;
+        double result = 0;
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("Input number");
+        int x = scanner.nextInt();
         System.out.println("Input number");
+        int y = scanner.nextInt();
+        
+        System.out.println("Choose operation(add/sub/mul/div)");
+        String op = scanner.nextLine();
+
+        switch (op) {
+            case "add":
+                result = x + y;
+                break;
+            case "sub":
+                result = x - y;
+                break;
+            default:
+                break;
+        }
+        
+        System.out.println("Result is: " + result);
+
     }
 }
